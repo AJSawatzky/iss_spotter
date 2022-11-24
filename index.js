04cb1d69739580d0f72323fe51228e2e9db0c62c
AJSawatzky <andrassawatzky@gmail.com>
2022-11-24 01:10:08 -0800

    first two iss spotter excercises.

---
+++ index.js
@@ -0,0 +1,19 @@
+const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsByIP('142.181.120.79', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log("It worked! Returned coordinates", coordinates);
+});
